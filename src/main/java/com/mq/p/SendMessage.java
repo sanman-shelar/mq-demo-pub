36f472dcc7ebe096a16bf574e1155da46fc2d802
Sanman Shelar <sanman.shelar@outlook.in>
2019-05-05 18:20:02 +0530

    Initial Commit

---
+++ src/main/java/com/mq/p/SendMessage.java
@@ -0,0 +1,28 @@
+package com.mq.p;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class SendMessage {
+
+  @Autowired
+  private JmsTemplate jmsTemplate;
+
+  @Value("${ibm.queue.name}")
+  private String QUEUE_NAME;
+  
+  @PostMapping("/send")
+  public void send(@RequestBody List<Person> persons) throws JmsException {    
+    System.out.println("Sending a transaction.");
+    jmsTemplate.convertAndSend(QUEUE_NAME, persons);
+  }
+}
